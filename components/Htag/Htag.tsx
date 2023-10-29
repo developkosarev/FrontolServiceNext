aff34116f8f887d875ce054f2ed407b66082ee8c
developkosarev <develop.kosarev@gmail.com>
2023-10-29 19:12:23 +0100

    Add overwiev page

--- components/Htag/Htag.tsx
+++ components/Htag/Htag.tsx
@@ -1,5 +1,14 @@
 import {HtagProps} from "./Htag.props";
 
 export const Htag = ({ tag, children }: HtagProps): JSX.Element => {
-    return (<h1 className="text-center">{ children }</h1>)
+    switch (tag) {
+        case 'h1':
+            return <h1 className="text-center">{ children }</h1>
+        case 'h2':
+            return <h2 className="text-center">{ children }</h2>
+        case 'h3':
+            return <h3 className="text-center">{ children }</h3>
+        default:
+            return <></>;
+    }
 }
