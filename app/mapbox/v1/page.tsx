30d33762faed0a574a7e1c43cada0b5570ac0883
developkosarev <develop.kosarev@gmail.com>
2023-10-30 22:29:42 +0100

    Add mapbox

---
+++ app/mapbox/v1/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Metadata } from "next";
+import { Htag } from "@/components/Htag/Htag";
+import MapboxAutofill from "@/components/MapboxAutofill/MapboxAutofill";
+
+export const metadata: Metadata = {
+    title: 'mapbox',
+    description: 'mapbox',
+    keywords: 'mapbox autofill'
+}
+
+export default function Page() {
+    return (
+        <div className="container">
+            <Htag tag='h1'>Autofill checkout demo (React)</Htag>
+          <MapboxAutofill></MapboxAutofill>
+        </div>
+    )
+}
