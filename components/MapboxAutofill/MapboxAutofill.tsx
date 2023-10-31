c1b4dec5794f3002dba4c8e83ee48727701ab1dc
developkosarev <develop.kosarev@gmail.com>
2023-10-31 12:03:30 +0100

    add styles

--- components/MapboxAutofill/MapboxAutofill.tsx
+++ components/MapboxAutofill/MapboxAutofill.tsx
@@ -2,8 +2,11 @@
 
 import React, { useState, useCallback, useEffect } from 'react';
 import { AddressAutofill, AddressMinimap, useConfirmAddress, config } from '@mapbox/search-js-react';
+import styles from './MapboxAutofill.module.scss'
 
 export default function MapboxAutofill() {
+    const MAPBOX_ACCESS_TOKEN: string = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
     const [showFormExpanded, setShowFormExpanded] = useState(false);
     const [showMinimap, setShowMinimap] = useState(false);
     const [feature, setFeature] = useState();
@@ -11,7 +14,7 @@ export default function MapboxAutofill() {
     const [token, setToken] = useState('');
 
     useEffect(() => {
-        const accessToken = 'MAPBOX_ACCESS_TOKEN';
+        const accessToken = `${MAPBOX_ACCESS_TOKEN}`;
         setToken(accessToken)
         config.accessToken = accessToken;
     }, [])
@@ -60,83 +63,87 @@ export default function MapboxAutofill() {
 
     return (
         <>
-            <form ref={formRef} className="flex flex--column" onSubmit={handleSubmit}>
-                <div className="grid grid--gut24 mb60">
-                    <div className="col col--auto-mm w-full">
+            <form ref={formRef} onSubmit={handleSubmit}>
+                <div className="mb-3">
+                    {/* Input form */}
+                    <label className="form-label">Address</label>
+                    <AddressAutofill accessToken={token} onRetrieve={handleRetrieve}>
+                        <input
+                            className="form-control"
+                            placeholder="Start typing your address, e.g. 123 Main..."
+                            autoComplete="address-line1"
+                            id="mapbox-autofill"
+                        />
+                    </AddressAutofill>
+                </div>
 
-                        {/* Input form */}
-                        <label className="txt-s txt-bold color-gray mb3">Address</label>
-                        <AddressAutofill accessToken={token} onRetrieve={handleRetrieve}>
-                            <input
-                                className="input mb12"
-                                placeholder="Start typing your address, e.g. 123 Main..."
-                                autoComplete="address-line1"
-                                id="mapbox-autofill"
-                            />
-                        </AddressAutofill>
-                        { !showFormExpanded &&
-                        <div
-                            id="manual-entry"
-                            className="w180 mt6 link txt-ms border-b color-gray color-black-on-hover"
-                            onClick={() => setShowFormExpanded(true)}
-                        >
-                            Enter an address manually
-                        </div>
-                        }
-                        <div className="secondary-inputs" style={{ display: showFormExpanded ? 'block' : 'none' }}>
-                            <label className="txt-s txt-bold color-gray mb3">Address Line 2</label>
-                            <input
-                                className="input mb12"
-                                placeholder="Apartment, suite, unit, building, floor, etc."
-                                autoComplete="address-line2"
-                            />
-                            <label className="txt-s txt-bold color-gray mb3">City</label>
-                            <input
-                                className="input mb12"
-                                placeholder="City"
-                                autoComplete="address-level2"
-                            />
-                            <label className="txt-s txt-bold color-gray mb3">State / Region</label>
-                            <input
-                                className="input mb12"
-                                placeholder="State / Region"
-                                autoComplete="address-level1"
-                            />
-                            <label className="txt-s txt-bold color-gray mb3">ZIP / Postcode</label>
-                            <input
-                                className="input"
-                                placeholder="ZIP / Postcode"
-                                autoComplete="postal-code"
-                            />
-                        </div>
+                { !showFormExpanded &&
+                    <div id="manual-entry"
+                        onClick={() => setShowFormExpanded(true)}
+                    >
+                        Enter an address manually
                     </div>
-                    <div className="col col--auto-mm">
-                        {/* Visual confirmation map */}
-                        <div
-                            id="minimap-container"
-                            className="h240 w360 relative mt18"
-                        >
-                            <AddressMinimap
-                                canAdjustMarker={true}
-                                satelliteToggle={true}
-                                feature={feature}
-                                show={showMinimap}
-                                onSaveMarkerLocation={handleSaveMarkerLocation}
-                            />
-                        </div>
+                }
+
+                <div className="mb-3" style={{ display: showFormExpanded ? 'block' : 'none' }}>
+                    <label className="form-label">Address Line 2</label>
+                    <input
+                        className="form-control"
+                        placeholder="Apartment, suite, unit, building, floor, etc."
+                        autoComplete="address-line2"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">City</label>
+                    <input
+                        className="form-control"
+                        placeholder="City"
+                        autoComplete="address-level2"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">State / Region</label>
+                    <input
+                        className="form-control"
+                        placeholder="State / Region"
+                        autoComplete="address-level1"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">ZIP / Postcode</label>
+                    <input
+                        className="form-control"
+                        placeholder="ZIP / Postcode"
+                        autoComplete="postal-code"
+                    />
+                </div>
+
+                <div className="mb-3">
+                    {/* Visual confirmation map */}
+                    <div
+                        id="minimap-container"
+                        className="h240 w360 relative mt18"
+                    >
+                        <AddressMinimap
+                            canAdjustMarker={true}
+                            satelliteToggle={true}
+                            feature={feature}
+                            show={showMinimap}
+                            onSaveMarkerLocation={handleSaveMarkerLocation}
+                        />
                     </div>
                 </div>
 
                 {/* Form buttons */}
                 { showFormExpanded &&
-                <div className="mb30 submit-btns">
-                    <button type="submit" className="btn round" id="btn-confirm">
-                        Confirm
-                    </button>
-                    <button type="button" className="btn round btn--gray-light ml3" id="btn-reset" onClick={resetForm}>
-                        Reset
-                    </button>
-                </div>
+                    <div className="mb-3">
+                        <button type="submit" className="btn btn-primary" id="btn-confirm">
+                            Confirm
+                        </button>
+                        <button type="button" className="btn btn-light" style={{marginLeft: 10}} id="btn-reset" onClick={resetForm}>
+                            Reset
+                        </button>
+                    </div>
                 }
             </form>
 
