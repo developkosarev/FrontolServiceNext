9d6082eac0dbe46490f0e59297f698aeae963948
developkosarev <develop.kosarev@gmail.com>
2024-09-22 08:34:27 +0200

    Fix story book

--- components/Htag/Htag.stories.ts
+++ components/Htag/Htag.stories.ts
@@ -14,7 +14,7 @@ const meta = {
   tags: ['autodocs'],
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { tag: "h1", children: "children" },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof Htag>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
