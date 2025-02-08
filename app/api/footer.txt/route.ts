fd55e0c6c0def9736a2f96ca8019dd235f20e514
developkosarev <develop.kosarev@gmail.com>
2025-02-08 20:33:10 +0100

    Add new api

---
+++ app/api/footer.txt/route.ts
@@ -0,0 +1,24 @@
+import { headers } from 'next/headers'
+import { Htag } from "@/components/Htag/Htag";
+
+const isExport = process.env.APP_ENV_EXPORT === 'production'
+
+export async function GET(request: Request) {
+  let referer: string = 'export';
+
+  if (!isExport) {
+    const headersList = headers()
+    referer = headersList.get('referer') as string
+  }
+
+  //const body = `<Htag tag='h1'>Renderer. Рендер</Htag>`
+  const body = `<H1>Renderer. Рендер</H1>`
+
+  return new Response(body, {
+        status: 200,
+        headers: {
+            referer: referer,
+            'Content-Type': 'text/html; charset=utf-8'
+        }
+      })
+}
